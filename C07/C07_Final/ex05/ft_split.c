3f235e72894101fe3e674dcf0df6aba81ba3918a
박원빈 <wbpark@Supreme-User.local>
2022-02-22 01:32:51 +0900

    "ft_split modified to NORM"

--- C07/C07_Final/ex05/ft_split.c
+++ C07/C07_Final/ex05/ft_split.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int	ft_is_charset(char *str, char c)
 {
@@ -14,31 +15,68 @@ int	ft_is_charset(char *str, char c)
 	return (0);
 }
 
-char	**ft_split(char *str, char *charset)
+char	*ft_strdup(char *str)
 {
-	char	**wdarr;
+	int	i;
+	int	j;
+	char	*dest;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	dest = (char *)malloc(sizeof(char) * i + 1);
+	if (!dest)
+		return (0);
+	j = 0;
+	while (str[j] != '\0')
+	{
+		dest[j] = str[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+char	**return_str(char *str, char **wdarr)
+{
+	wdarr[0] = ft_strdup(str);
+	wdarr[1] = 0;
+	return (wdarr);
+}
+
+char	**split_str(char *str, char **wdarr, char *charset)
+{
 	int	i;
 	int	j;
 	int	k;
-
-	wdarr = (char **)malloc(sizeof(char) * 1000);
+	
+	if (charset[0] == '\0')
+		return (return_str(str, wdarr));
 	i = 0;
 	k = 0;
+	if (ft_is_charset(charset, str[i]) == 1)
+		i++;
 	while (str[i] != '\0')
 	{
-		if (ft_is_charset(charset, str[i]) == 1)
-			i++;
-		j = 0;
-		while (ft_is_charset(charset, str[i]) == 0)
+		if (ft_is_charset(charset, str[i]) == 0)
 		{
+			j = 0;
 			wdarr[k] = (char *)malloc(sizeof(char) * 1000);
-			wdarr[k][j] = str[i];
-			j++;
-			i++;
+			while (ft_is_charset(charset, str[i]) == 0)
+				wdarr[k++][j] = str[i++];
+			wdarr[k++][j] = '\0';
 		}
-		wdarr[k][j] = '\0';
-		i++;
-		k++;
+		else
+			i++;
 	}
+	wdarr[k] = 0;
 	return (wdarr);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**wdarr;
+
+	wdarr = (char **)malloc(sizeof(char) * 1000);
+	return (split_str(str, wdarr, charset));
 }
